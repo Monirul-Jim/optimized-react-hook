ca6aec215cc52a888fb6c53afead7c6ed4412538
unknown <Monirul-Jim>
2024-01-09 21:00:58 +0600

    learn clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
+import FetchCleanup from "./FetchCleanup";
 import InputForm from "./InputForm";
-import UseEffectExample from "./UseEffectExample";
+// import UseEffectExample from "./UseEffectExample";
 import UseReducerExample from "./UseReducerExample";
 import UseReducerWithTypeScript from "./UseReducerWithTypeScript";
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <InputForm />
       <UseReducerExample />
       <UseReducerWithTypeScript />
-      <UseEffectExample />
+      {/* <UseEffectExample /> */}
+      <FetchCleanup />
     </>
   );
 }
---
+++ src/FetchCleanup.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+const FetchCleanup = () => {
+  return <div></div>;
+};
+const Todo = () => {
+  useEffect(() => {}, []);
+  return <div className="border border-red-500 p-10"></div>;
+};
+export default FetchCleanup;
